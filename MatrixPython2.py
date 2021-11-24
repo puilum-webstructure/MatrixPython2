66bbf9e62b9801f2459c7588ba168250c1e5d5a0
puilum-webstructure <puilum@webstruc.net>
2021-11-24 12:23:34 +0800

    created MatrixPython2

--- MatrixPython2.py
+++ MatrixPython2.py
@@ -1,23 +1,53 @@
+import multiprocessing 
 
 # 1D array matrix addition 
 def addition_1D(lst1, lst2): 
-    # check if it's a one dimension array 
+    # check if both matrix are of the same length
+    if len(lst1) != len(lst2): 
+        raise Exception("Lenght of both matrix needs to be the same.")
 
-    # check if all values are in type integer 
-    pass
-    
+    # check if it's a one dimension integer only array 
+    for i in range(len(lst1)): 
+        x = isinstance(lst1[i], int)
+        if not x:
+            raise Exception("Matrix 1 should be of type integer and is one dimensional only")
+   
+    for i in range(len(lst1)): 
+        x = isinstance(lst1[i], int)
+        if not x:
+            raise Exception("Matrix 2 should be of type integer and is one dimensional only")
+   
+    result = []
+    for i in range(len(lst1)):
+        result.append(lst1[i] + lst2[i])
 
-# 2D array matrix addition
-def addition_2D(lst1, lst2): 
-    # check if it's a two dimension array 
+    return result 
 
-    # check if all values are in type integer 
-    pass
-# 2D array matrix multiplication 
-def addition_1D(lst1, lst2): 
-    # check if it's a two dimension array 
+if __name__ == "__main__": 
+    lst1 = [3,4,5]
+    lst2 = [7,7,3] 
+    print(addition_1D(lst1, lst2))
 
-    # check if all values are in type integer 
-    pass
+#processes = []
+#for _ in range(len(lst1)): 
+#      p = multiprocessing.Process(target = addition_1D(lst1, lst2))
+#      p.start()
+#      processes.append(p)
 
-# testing comment 
+#for process in processes: 
+#    process.join()
+
+## 2D array matrix addition
+#def addition_2D(lst1, lst2): 
+#    # check if it's a two dimension array 
+
+#    # check if all values are in type integer 
+#    pass
+## 2D array matrix multiplication 
+#def addition_1D(lst1, lst2): 
+#    # check if it's a two dimension array 
+
+#    # check if all values are in type integer 
+#    pass
+
+## testing comment 
