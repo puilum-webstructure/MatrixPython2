c8be3d244634f18589fa292f2c16a555a0d2f19e
puilum-webstructure <puilum@webstruc.net>
2021-11-24 10:11:11 +0800

    created MatrixPython2

---
+++ MatrixPython2.py
@@ -0,0 +1,23 @@
+
+# 1D array matrix addition 
+def addition_1D(lst1, lst2): 
+    # check if it's a one dimension array 
+
+    # check if all values are in type integer 
+    pass
+    
+
+# 2D array matrix addition
+def addition_2D(lst1, lst2): 
+    # check if it's a two dimension array 
+
+    # check if all values are in type integer 
+    pass
+# 2D array matrix multiplication 
+def addition_1D(lst1, lst2): 
+    # check if it's a two dimension array 
+
+    # check if all values are in type integer 
+    pass
+
+
