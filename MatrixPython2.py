c8617a74272b40520a3d33bd4cc27150ec505d24
puilum-webstructure <puilum@webstruc.net>
2021-11-26 15:58:37 +0800

    updated version of Matrix with threading

--- MatrixPython2.py
+++ MatrixPython2.py
@@ -1,7 +1,8 @@
-import multiprocessing 
+ 
 
 # 1D array matrix addition 
 def addition_1D(lst1, lst2): 
+    
     # check if both matrix are of the same length
     if len(lst1) != len(lst2): 
         raise exception("lenght of both matrix needs to be the same.")
@@ -18,29 +19,11 @@ def addition_1D(lst1, lst2):
             raise exception("matrix 2 should be of type integer and is one dimensional only")
    
     result = []
-    #value = lst1 + lst2
-    #result.append(value)
     for i in range(len(lst1)):
         value = lst1[i] + lst2[i] 
         result.append(value)
 
-    # print(result)  #[10,11,8] 
     return result
-
-#if __name__ == "__main__": 
-#    lst1 = [3,4,5]
-#    lst2 = [7,7,3] 
-#    print(addition_1d(lst1, lst2))
-
-#    processes = []
-#    for i in range(3): 
-#          p = multiprocessing.process(target = addition_1d(lst1[i], lst2[i]))
-#          p.start()
-#          processes.append(p)
-
-#    for process in processes: 
-#        process.join()
-
 
 
 #2d array matrix addition
@@ -134,7 +117,10 @@ def multiplication_2D(lst1, lst2):
 if __name__ == "__main__": 
     lst1 = [3,4,5]
     lst2 = [7,7,3] 
+    start = timer()
     print("Output for 1D addition" + str(addition_1D(lst1, lst2)))
+    end = timer()
+    print(end-start)
 
     lst1 = [[1,2],[3,4]]
     lst2 = [[0,1],[4,5]]
@@ -144,106 +130,87 @@ if __name__ == "__main__":
     lst2 = [[0,1],[4,5]]
     print("Output for 2D multiplication" + str(multiplication_2D(lst1, lst2)))
 
-#Output for 1D addition[10, 11, 8]
-#Output for 2D addition[[1, 3], [7, 9]]
-#Output for 2D multiplication[[8, 11], [16, 23]]
-"""
-git commands 
-
-cd /c 
-cd Users/
-cd puilum/ 
-cd git/
-$ git config --global user.name "puilum-webstructure"
-$ git config --global user.email puilum@webstruc.net
-$ git clone https://github.com/puilum-webstructure/MatrixPython2.git
-git status 
-$ git commit -m"created MatrixPython2" MatrixPython2.py
-$ git push -u origin HEAD:master
-"""
 
 # implementing multithreading version
 import threading 
-import time
+#import concurrent.futures 
+from timeit import default_timer as timer 
 
 # addition 1d
-matrix1 = [3,4,5]
-matrix2 = [7,7,3] 
-res = []
-for i in range(len(matrix1)): 
-    res.append(0)
 def threading_addition_1D(n):  
     res[n] = matrix1[n] + matrix2[n]
-   
-for i in range(len(matrix1)): 
-    x = threading.Thread(target=threading_addition_1D, args = (i,))
-    x.start()
-    x.join()
-print(res)
 
-# addition 2d
+#matrix1 = [3,4,5]
+#matrix2 = [7,7,3] 
+#start = timer()
+#with concurrent.futures.ThreadPoolExecutor() as executor: 
+#    for i in range(len(matrix1)):
+#        f1 = executor.submit(threading_addition_1D, i)
+    
+#    print(res)
 
-matrix1 = [[1,2],[3,4]]
-matrix2 = [[0,1],[4,5]]  
-res = [[0 for x in range(2)] for y in range(2)] 
+#end = timer()
+#print(end-start)
+
 def threading_addition_2D(i, j): 
+    '''
+    function for 2x2 matrix additionn using Threading 
+    '''
     res[i][j] = matrix1[i][j] + matrix2[i][j]
 
-for i in range(2): 
-    for j in range(2): 
-        x = threading.Thread(target=threading_addition_2D, args = (i,j,k,))
-        x.start()
-        x.join()
-print(res)
 
-
-# multiplication 2d
-
-matrix1 = [[1,2],[3,4]]
-matrix2 = [[0,1],[4,5]]  
-res = [[0 for x in range(2)] for y in range(2)] 
 def threading_multiplication_2D(i,j,k): 
-   res[i][j] += lst1[i][k] * lst2[k][j]
-
-for i in range(2): 
-        for j in range(2): 
-            for k in range(2): 
-                x = threading.Thread(target=threading_multiplication_2D, args = (i,j,k, ))
-                x.start()
-                x.join()
-print(res) #[[8, 11], [16, 23]]
-
-
-'''
-# explicit for loops
-     for i in range(len(lst1)): 
-        for j in range(len(lst2[0])): 
-            for k in range(len(lst2)): 
-  
-                # resulted matrix
-                final_output[i][j] += lst1[i][k] * lst2[k][j]
-     return final_output
-'''
+    '''
+    function for 2x2 matrix multiplication using Threading 
+    '''
+    res[i][j] += matrix1[i][k] * matrix2[k][j]
+   
 
 if __name__ == "__main__":
-    # creating thread
-    A1 = threading.Thread(target=threading_addition_1D, args=(10,))
-    A2 = threading.Thread(target=threading_addition_2D, args=(10,))
-    M2 = threading.Thread(target=threading_multiplication_2D, args=(10,))
-  
-    # starting thread 1
-    A1.start()
-    # starting thread 2
-    A2.start()
-    # starting thread 2
-    M2.start()
-  
-    # wait until thread 1 is completely executed
-    A1.join()
-    # wait until thread 2 is completely executed
-    A2.join()
-    # wait until thread 3 is completely executed
-    M2.join()
-  
-    # both threads completely executed
-    print("Done!")
+    # creating threads
+    # threads for addition 1D matrix 
+    matrix1 = [3,4,5]
+    matrix2 = [7,7,3] 
+    res = [0 for x in range(len(matrix1))]
+    
+    start = timer()
+    threads = []
+    for i in range(len(matrix1)): 
+        x = threading.Thread(target=threading_addition_1D, args = (i,))
+        x.start()
+        threads.append(x) 
+
+    for thread in threads: 
+        thread.join()
+    print("Output for 1D addition using threading" + str(res))
+    end = timer() 
+    print(end-start)
+
+    # threads for addition 2D matrix 
+    matrix1 = [[1,2],[3,4]]
+    matrix2 = [[0,1],[4,5]]  
+    res = [[0 for x in range(2)] for y in range(2)] 
+
+    for i in range(2): 
+        for j in range(2): 
+            x = threading.Thread(target=threading_addition_2D, args = (i,j,))
+            x.start()
+            x.join()
+    print("Output for 2D addition using threading" + str(res))
+
+
+    # threads for multiplication 2D matrix 
+    matrix1 = [[1,2],[3,4]]
+    matrix2 = [[0,1],[4,5]]  
+    res = [[0 for x in range(2)] for y in range(2)] 
+
+    for i in range(2): 
+            for j in range(2): 
+                for k in range(2): 
+                    x = threading.Thread(target=threading_multiplication_2D, args = (i,j,k, ))
+                    x.start()
+                    x.join()
+    print("Output for 2D multiplication using threading" + str(res))
+
+
+# https://medium.com/python-experiments/parallelising-in-python-mutithreading-and-mutiprocessing-with-practical-templates-c81d593c1c49
